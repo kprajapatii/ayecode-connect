fa1d58cf5c2e92ac6e44e954bc2919a20efef615
stiofan <stiofansisland@gmail.com>
2024-07-17 13:46:26 +0100

    1.3.2

--- ayecode-connect.php
+++ ayecode-connect.php
@@ -4,7 +4,7 @@
  * Plugin Name: AyeCode Connect
  * Plugin URI: https://ayecode.io/
  * Description: A service plugin letting users connect AyeCode Services to their site.
- * Version: 1.3.1
+ * Version: 1.3.2
  * Author: AyeCode
  * Author URI: https://ayecode.io
  * Requires at least: 4.7
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( !defined( 'AYECODE_CONNECT_VERSION' ) ) {
-	define( 'AYECODE_CONNECT_VERSION', '1.3.1' );
+	define( 'AYECODE_CONNECT_VERSION', '1.3.2' );
 }
 
 if ( !defined( 'AYECODE_CONNECT_SSL_VERIFY' ) ) {
