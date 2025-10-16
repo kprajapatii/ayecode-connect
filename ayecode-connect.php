aae49a474ab6a5095589c1346d6776fdd779b5e5
Kiran Prajapati <officeofkiran@gmail.com>
2025-10-16 19:58:08 +0530

    1.4.13

--- ayecode-connect.php
+++ ayecode-connect.php
@@ -3,7 +3,7 @@
  * Plugin Name: AyeCode Connect
  * Plugin URI: https://ayecode.io/
  * Description: A service plugin letting users connect AyeCode Services to their site.
- * Version: 1.4.12
+ * Version: 1.4.13
  * Author: AyeCode
  * Author URI: https://ayecode.io
  * Requires at least: 5.0
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( !defined( 'AYECODE_CONNECT_VERSION' ) ) {
-	define( 'AYECODE_CONNECT_VERSION', '1.4.12' );
+	define( 'AYECODE_CONNECT_VERSION', '1.4.13' );
 }
 
 if ( !defined( 'AYECODE_CONNECT_SSL_VERIFY' ) ) {
